abb14fcdb5eca9503b6344fec4be8bec1b1533b3
XiWen Jiang <1091520986@qq.com>
2017-02-05 01:05:03 +0800

    v1.0.0
    把对GitHub客户端操作不熟练而导致的未同步代码贴上，并且修复了点击评论会导致一次比一次多的错误

--- script/index.js
+++ script/index.js
@@ -3,197 +3,244 @@ var currentPage = 1;
 var maxPage;
 function ShowMessage(type,message)
 {
-    var iconURL;
-    var backgroundColor;
-    if(type === "error")
-    {
-        iconURL = "./image/error.png";
-        backgroundColor = "red";
-    }
-    else if(type === "success")
-    {
-        iconURL = "./image/success.png";
-        backgroundColor = "yellowgreen";
-    }
-    else
-        return;
-    var node = '<div id="message" style="display:none;z-index:auto;background-color:' + backgroundColor + '"><img src="' + iconURL + '" />' + message +'</div>';
-    $message = $(node).prependTo("body");
-    $message.fadeIn(2000,function(){
-        $message.fadeOut(2000,function(){
-            $message.remove();
-        });
-    });
+	var iconURL;
+	var backgroundColor;
+	if(type === "error")
+	{
+		iconURL = "./image/error.png";
+		backgroundColor = "red";
+	}
+	else if(type === "success")
+	{
+		iconURL = "./image/success.png";
+		backgroundColor = "yellowgreen";
+	}
+	else
+		return;
+	var node = '<div class="message" style="display:none;z-index:auto;background-color:' + backgroundColor + '"><img src="' + iconURL + '" />' + message +'</div>';
+	$message = $(node);
+	$("#message").html($message);
+	$message.fadeIn(2000,function(){
+		$message.fadeOut(2000);
+	});
+	/*
+	$("#message").click(function(){
+		$(this).remove();
+		$("#message").off("click");
+	});
+	*/
 }
 
 function GetMaxPage()
 {
 	$.ajax({
-		url:"GetListNum.php",
+		url:"GetItemNum.php",
 		async: false,
-		success:function(data){
-			json = data;
+		dataType:"json",
+		success:function(rtnData){
+			if(rtnData["status"] === "success")
+				maxPage = Math.ceil(rtnData["info"]["message"] / itemsPerPage);
+			else
+				ShowMessage("error","发生错误:" + rtnData["info"]["message"] + ",错误代码：" + rtnData["info"]["code"]);
+		},
+		error:function(XMLHttpRequest, textStatus, errorThrown){
+			if(textStatus == "parseerror")
+				ShowMessage("error","发生错误:返回数据格式有误,错误代码:3101");
+			else if(textStatus == "timeout")
+				ShowMessage("error","发生错误:与服务器失去联系,错误代码:3102");
+			else 
+				ShowMessage("error","发生错误:接受返回数据时发生错误,错误代码:3103");
 		}
+
 	});
-	if($.parseJSON(json)["status"] === "success")
-		maxPage = Math.ceil($.parseJSON(json)["info"]["message"] / itemsPerPage);
-	else
-		ShowMessage("error","获取表白列表时发生错误！");
+}
+
+function ConstructCommentNode(comment)
+{
+	var node = 
+		'<div id="loveComment_' + comment.CommentID + '">' +
+		'<div id="loveComment_time_' + comment.CommentID + '">评论时间：' + comment.SubmitTime + '</div>' +
+		'<div id=loveComment_words_' + comment.CommentID + '">' +  comment.Context  + '</div>' +
+		'<div id="loveComment_user_' + comment.CommentID + '">' + comment.SubmitUser + '</div>' +
+		'</div>';
+	return node;
+}
+
+function AddItemComment(itemID,comment)
+{
+	var node = ConstructCommentNode(comment);
+	$("#loveComments_" + itemID).append(node);
+}
+
+function SubmitComment()
+{
+	$(document).delegate("[id^=commentArea_submit_]","click",function(){
+		var itemID = $(this).attr("id").substr(19);
+		var commentContext = $("#commentArea_context_" + itemID).val();
+		if(commentContext == "")
+		{
+			ShowMessage("error","评论不可为空");
+			return;
+		}
+		$.ajax({
+			url:"SubmitComment.php",
+			type:"POST",
+			async:false,
+			dataType:"json",
+			data:{itemID:itemID,context:commentContext},
+			success:function(rtnData){
+				if(rtnData["status"] != "success")
+				{
+					ShowMessage("error","发生错误:" + rtnData["info"]["message"] + ",错误代码：" + rtnData["info"]["code"]);
+				}
+				else
+				{
+					ShowMessage("success","发表成功");
+					var newComment = rtnData["info"]["message"][0];
+					var node = ConstructCommentNode(newComment);
+					$(node).prependTo("#loveComments_" + itemID).hide().slideDown("slow");
+					$("#commentArea_context_" + itemID).val("");
+					$("#commentNum_" + itemID).html(parseInt($("#commentNum_" + itemID).html())+1);
+					//alert($("#loveOperation_comment_" + itemID).val("4"));
+				}
+			},
+			error:function(XMLHttpRequest, textStatus, errorThrown){
+				if(textStatus == "parseerror")
+					ShowMessage("error","发生错误:返回数据格式有误,错误代码:3101");
+				else if(textStatus == "timeout")
+					ShowMessage("error","发生错误:与服务器失去联系,错误代码:3102");
+				else 
+					ShowMessage("error","发生错误:接受返回数据时发生错误,错误代码:3103");
+			}
+		});
+	});
+}
+
+function SetItemComments()
+{
+	$(document).delegate("[id^=loveOperation_comment_]","click",function(){
+		var itemID = $(this).attr("id").substr(22);
+		$("#commentArea_" + itemID).slideToggle("slow");
+
+		if($("#loveComments_" + itemID).html() != "")
+			return;
+
+		$.ajax({
+			url:"GetComments.php",
+			type:"GET",
+			async:false,
+			dataType:"json",
+			data:"itemID=" + itemID,
+			success:function(rtnData){
+				if(rtnData["status"] != "success")
+				{
+					ShowMessage("error","获取数据时发生错误");
+					$("#loveComments_" + itemID).html("获取数据时发生错误");
+				}
+				var comments = rtnData["info"]["message"];
+				$.each(comments,function(index,item){
+					AddItemComment(itemID,item);
+				});
+			},
+			error:function(XMLHttpRequest, textStatus, errorThrown){
+				if(textStatus == "parseerror")
+					ShowMessage("error","发生错误:返回数据格式有误,错误代码:3101");
+				else if(textStatus == "timeout")
+					ShowMessage("error","发生错误:与服务器失去联系,错误代码:3102");
+				else 
+					ShowMessage("error","发生错误:接受返回数据时发生错误,错误代码:3103");
+			}
+
+		});
+	});
+}
+
+function ConstructItemNode(item)
+{
+	var node = 
+		'<div id="loveItem_' + item.ItemID + '">' +
+		'<div id="loveTime_' + item.ItemID + '" class="loveTime">表白时间：' + item.SubmitTime + '</div>' +
+		'<div id="loveTarget_' + item.ItemID + '" class="loveTarget">Dear ' + item.TargetName + '</div>' +
+		'<div id="loveWords_' + item.ItemID + '" class="loveWords">' + item.Context + '</div>' +
+		'<div id="loveUser_' + item.ItemID + '" class="loveUser">From ' + item.SubmitUser + '</div>' +
+		'<div id="loveOperation_' + item.ItemID + '">' + 
+		'<div id="loveOperation_up_' + item.ItemID + '" class="inline handCursor littleMargin">'+ "赞(" + '<div id="upNum_' + item.ItemID + '" class="inline">' + item.UpNum + '</div>)</div>'+
+		'<div id="loveOperation_comment_' + item.ItemID + '"  class="inline handCursor littleMargin">' + '评论(' + '<div id="commentNum_' + item.ItemID + '" class="inline">' + item.CommentNum + '</div>)</div>' +
+		'</div>' +
+		'<div id="commentArea_' + item.ItemID + '" style="display:none;">' + 
+		'<textarea id="commentArea_context_' + item.ItemID + '" name="context" class="loveComment wideTextArea littleMargin autoMargin littleBorderRadius" row =100 col=100></textarea>' +
+		'<div id="commentArea_submit_' + item.ItemID + '"  class="loveComment_Submit handCursor centerAligin autoMargin littleMargin">提交</div>' +
+		'<div id="loveComments_' + item.ItemID +'">' + '</div>' +
+		'</div>' +
+		'</div>';
+	return node;
+}
+
+function AddListItem(item)
+{
+	var node = ConstructItemNode(item);
+	$("#listArea").append(node);
 }
 
 function SetList(page)
-{       /*
-	if(page < 1)
-	{
-		ShowMessage("error","已经是第一页了！");
+{       
+	if(page <= maxPage)
+		window.currentPage = page;
+	else
 		return false;
-	}
-	else if(page > maxPage)
-	{
-                if(maxPage != 0)
-                    ShowMessage("error","已经是最后一页了！");
-		return false;
-	}
-        */
-	//$("#list").html("正在加载...");
-        if(page <= maxPage)
-            window.currentPage = page;
-        else
-            return false;
 	$.ajax({
-		url:"GetList.php",
+		url:"GetItems.php",
 		type:"GET",
-		data:"page=" + page + "&lpp=" + itemsPerPage,
+		dataType:"json",
+		data:{page:page,lpp:itemsPerPage},
 		async:false,
 		success:function(rtnData)
 		{
-			if($.parseJSON(rtnData)["status"] !== "success")
-                        {
-                                ShowMessage("error","获取数据时发生错误");
-                                $("#listArea").html("获取数据时发生错误");
-				return false;
-                        }
-			rtnData_json = $.parseJSON(rtnData)["info"]["message"];
-			$.each(rtnData_json,function(index,item){
-				var node = 
-					'<div id="loveItem_' + item.id + '">' +
-                                            '<div id="loveTime_' + item.id + '" class="loveTime">' + item.SubmitTime + '</div>' +
-                                            '<div id="loveTarget_' + item.id + '" class="loveTarget">Dear ' + item.TargetName + '</div>' +
-                                            '<div id="loveWords_' + item.id + '" class="loveWords">' + item.Context + '</div>' +
-                                            '<div id="loveUser_' + item.id + '" class="loveUser">From ' + item.SubmitUser + '</div>' +
-                                            '<hr/>' +
-                                            '<div id="loveItem_OthersOperation_' + item.id + '">' + 
-                                                '<div id="loveItem_OthersOperation_up_' + item.id + '" class="loveItem_OthersOperation">'+ "赞</div>"+
-                                                '<div id="loveItem_OthersOperation_comment_' + item.id + '" class="loveItem_OthersOperation">'+ "评论</div>"
-                                            '</div>' +
-					'</div>';
-//				if(index==0)
-//					$("#list").empty();
-				$("#listArea").append(node); 
+			if(rtnData["status"] !== "success")
+			{
+				ShowMessage("error","获取数据时发生错误");
+				$("#listArea").html("获取数据时发生错误");
+			}
+			var items = rtnData["info"]["message"];
+			$.each(items,function(index,item){
+				AddListItem(item);
 			});
+		},
+		error:function(XMLHttpRequest, textStatus, errorThrown){
+			if(textStatus == "parseerror")
+				ShowMessage("error","发生错误:返回数据格式有误,错误代码:3101");
+			else if(textStatus == "timeout")
+				ShowMessage("error","发生错误:与服务器失去联系,错误代码:3102");
+			else 
+				ShowMessage("error","发生错误:接受返回数据时发生错误,错误代码:3103");
 		}
-	});
-	return true;
-}
 
-/*
-function SetControlBar()
-{
-        $("#controlBar").undelegate("click","[div[id^=toPage_]]");
-        $("#controlBar").empty();
-        if(currentPage>1)
-            $("#controlBar").append('<div id="lastPage">上一页</div>');
-        
-	if(currentPage<=5)
-	{
-		for(var i=1;i<=11 && i<=maxPage;++i)
-		{
-			var node ='<div id="toPage_' + i + '" >' + i + '</div>';
-			$("#controlBar").append(node);
-		}
-	}
-	else if(currentPage>maxPage-5)
-	{
-		for(var i=maxPage - 11;i<=maxPage;++i)
-		{
-			var node ='<div id="toPage_' + i + '" >' + i + '</div>';
-			$("#controlBar").append(node);
-		}
-	}
-	else
-	{
-		for(var i=currentPage - 5;i<=current + 5;++i)
-		{
-			var node ='<div id="toPage_' + i + '" >' + i + '</div>';
-			$("#controlBar").append(node);
-		}
-	}
-	$("div[id^=toPage_]").each(function(index,item){
-		$("#controlBar").delegate("#" + $(item).attr("id"),"click",function(){
-                    if($(item).attr("id") !== "toPage_"+currentPage)
-                    {
-			SetList(index + 1);
-                        $("html,body").animate({scrollTop:$("#board").offset().top},1000);
-                        SetControlBar();
-                    }
-		});
 	});
-        
-        if(currentPage < maxPage)
-            $("#controlBar").append('<div id="nextPage">下一页</div>');
 }
-*/
 
 var isSending = false;
-$(document).ready(function(){
-	GetMaxPage();
-	SetList(currentPage);
-        /*
-	SetControlBar();
-        
-        $("#controlBar").delegate("#lastPage","click",function(){
-		SetList(currentPage-1);
-                $("html,body").animate({scrollTop:$("#board").offset().top},1000);
-                SetControlBar();
-        });
-        
-        $("#controlBar").delegate("#nextPage","click",function(){
-		SetList(currentPage+1);
-                $("html,body").animate({scrollTop:$("#board").offset().top},1000);
-                SetControlBar();
-        });  
-        */
-        $("#loadingBar").append("<div>拉到底加载哦</div>");
-        $(function(){
-            $(window).scroll(function() {
-                if ($(this).scrollTop() + $(window).height() + 20 >= $(document).height() && $(this).scrollTop() > 20) {
-                    $("#loadingBar").html('<div><img src="image/loading.gif" />正在加载...</div>');
-                    if(SetList(currentPage + 1))
-                        $("#loadingBar").html("<div>拉到底加载哦</div>");
-                    else
-                        $("#loadingBar").html("<div>已经到底了哦</div>");
-                }
-            });
-        });
+function Submit()
+{
 	$("#submitArea_submit").click(function(){
-                if($("#submitArea_context").val()==="")
-                {
-                    ShowMessage("error","表白内容不可为空");
-                    return;
-                }
-                if($("#submitArea_targetName").val()==="")
-                {
-                    ShowMessage("error","表白对象不可为空");
-                    return;
-                }
-                if(!isSending)
-                    isSending = true;
-                else
-                {
-                    ShowMessage("error","请勿重复请求");
-                    return;
-                }
-                $(this).html("正在发布..");
+		if($("#submitArea_context").val()==="")
+		{
+			ShowMessage("error","表白内容不可为空");
+			return;
+		}
+		if($("#submitArea_targetName").val()==="")
+		{
+			ShowMessage("error","表白对象不可为空");
+			return;
+		}
+		if(!isSending)
+			isSending = true;
+		else
+		{
+			ShowMessage("error","请勿重复请求");
+			return;
+		}
+
+		$(this).html("正在发布..");
 		var isAnonymous;
 		if($("#submitArea_isAnonymous").is(":checked"))
 			isAnonymous = "on";
@@ -202,18 +249,86 @@ $(document).ready(function(){
 		$.ajax({
 			url:"submit.php",
 			type:"POST",
+			dataType:"json",
 			data:{context:$("#submitArea_context").val(),targetName:$("#submitArea_targetName").val(),isAnonymous:isAnonymous},
+			async:false,
 			success:function(rtnData){
-				if($.parseJSON(rtnData)["status"] !== "success")
-					ShowMessage("error","发生错误:" + $.parseJSON(rtnData)["info"]["message"] + ",错误代码：" + $.parseJSON(rtnData)["info"]["code"]);
+				//				rtnData = $.parseJSON(rtnData);
+				if(rtnData["status"] !== "success")
+					ShowMessage("error","发生错误:" + rtnData["info"]["message"] + ",错误代码：" + rtnData["info"]["code"]);
 				else
 				{
 					ShowMessage("success","发表成功");
-					location.reload();
+					var item = rtnData["info"]["message"][0];
+					var node = ConstructItemNode(item);
+					$(node).prependTo("#listArea").hide().slideDown("slow");
+					isSending = false;
+					$("#submitArea_submit").html("发布");
 				}
+			},
+			error:function(XMLHttpRequest, textStatus, errorThrown){
+				if(textStatus == "parseerror")
+					ShowMessage("error","发生错误:返回数据格式有误,错误代码:3101");
+				else if(textStatus == "timeout")
+					ShowMessage("error","发生错误:与服务器失去联系,错误代码:3102");
+				else 
+					ShowMessage("error","发生错误:接受返回数据时发生错误,错误代码:3103");
 			}
 		});
-                isSending = false;
-                $(this).html = "发布";
 	});
+}
+
+function SubmitUp()
+{
+	$(document).delegate("[id^=loveOperation_up_]","click",function(){
+		var itemID = $(this).attr("id").substr(17);
+		$.ajax({
+			url:"SubmitUp.php",
+			type:"POST",
+			dataType:"json",
+			async:false,
+			data:{itemID:itemID},
+			success:function(rtnData){
+				if(rtnData["status"] !== "success")
+					ShowMessage("error","发生错误:" + rtnData["info"]["message"] + ",错误代码：" + rtnData["info"]["code"]);
+				else
+				{
+					ShowMessage("success","点赞成功");
+					$("#upNum_" + itemID).html(parseInt($("#upNum_" + itemID).html()) + 1);
+				}
+			},
+			error:function(XMLHttpRequest, textStatus, errorThrown){
+				if(textStatus == "parseerror")
+					ShowMessage("error","发生错误:返回数据格式有误,错误代码:3101");
+				else if(textStatus == "timeout")
+					ShowMessage("error","发生错误:与服务器失去联系,错误代码:3102");
+				else 
+					ShowMessage("error","发生错误:接受返回数据时发生错误,错误代码:3103");
+			}
+
+		});
+	});
+}
+
+
+
+$(document).ready(function(){
+	GetMaxPage();
+	SetList(currentPage);
+	$("#loadingBar").append("<div>拉到底加载哦</div>");
+	$(function(){
+		$(window).scroll(function() {
+			if ($(this).scrollTop() + $(window).height() + 20 >= $(document).height() && $(this).scrollTop() > 20) {
+				$("#loadingBar").html('<div><img src="image/loading.gif" />正在加载...</div>');
+				if(SetList(currentPage + 1))
+					$("#loadingBar").html("<div>拉到底加载哦</div>");
+				else
+					$("#loadingBar").html("<div>已经到底了哦</div>");
+			}
+		});
+	});
+	Submit();
+	SetItemComments();
+	SubmitComment();
+	SubmitUp();
 });
